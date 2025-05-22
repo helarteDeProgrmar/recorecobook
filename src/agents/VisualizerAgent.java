b7a4a4daf90b9e11cdfdb5821b5834e4d596415b
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-05-22 18:52:49 +0200

    feat: added a interface basic to implement the ui

--- src/agents/VisualizerAgent.java
+++ src/agents/VisualizerAgent.java
@@ -7,100 +7,62 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 import jade.lang.acl.MessageTemplate;
 import jade.core.behaviours.OneShotBehaviour;
-// import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.CyclicBehaviour;
 
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import models.Book;
+import ui.VisualizerUI;
+import javax.swing.SwingUtilities;
 
 public class VisualizerAgent extends Agent {
 
+    private VisualizerUI ui;
+
     protected void setup() {
         System.out.println("VisualizerAgent started.");
-        Scanner scanner = new Scanner(System.in);
-        FSMBehaviour fsm = new FSMBehaviour(this);
+        SwingUtilities.invokeLater(() -> {
+            ui = new VisualizerUI(this);
+            ui.setVisible(true);
+        });
 
-        fsm.registerFirstState(new OneShotBehaviour() {
+        // 2) Comportamiento permanente para recibir recomendaciones
+        addBehaviour(new CyclicBehaviour() {
+            private MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+
+            @Override
             public void action() {
-
-                // Genres to rate
-                String[] genres = {
-                        "action", "fantasy", "romance", "horror", "science_fiction", "drama", "comedy"
-                };
-
-                Map<String, Integer> preferences = new HashMap<>();
-
-                System.out.println("Rate your interest in the following genres (1 to 10):");
-
-                for (String genre : genres) {
-                    int value = -1;
-                    while (value < 1 || value > 10) {
-                        System.out.print(genre + ": ");
-                        try {
-                            value = Integer.parseInt(scanner.nextLine());
-                        } catch (NumberFormatException e) {
-                            value = -1;
-                        }
-                    }
-                    preferences.put(genre, value);
-                }
-
-                System.out.print("Enter an author to search: ");
-                String string_authors = scanner.nextLine();
-
-                List<String> authors = Arrays.stream(string_authors.split(";")).map(String::trim)
-                        .collect(Collectors.toList());
-                System.out.println(authors.toString());
-                // Send data to RecommenderAgent
-                ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-                msg.addReceiver(new AID("recommender", AID.ISLOCALNAME));
-                try {
-                    msg.setContentObject(new Object[] { preferences, authors });
-                    send(msg);
-                    System.out.println("Preferences and author sent to recommender.");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, "GET_INPUT");
-
-        // Behavior to receive recommendations
-        fsm.registerState(new OneShotBehaviour() {
-            public void action() {
-                // MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-                // ACLMessage reply = receive(mt);
-                ACLMessage reply = blockingReceive(
-                        MessageTemplate.MatchPerformative(ACLMessage.INFORM));
-
-                if (reply != null) {
+                ACLMessage msg = receive(mt);
+                if (msg != null) {
                     try {
-                        Object content = reply.getContentObject();
-                        if (content instanceof List<?>) {
-                            System.out.println("\nRecommended books:");
-                            for (Object obj : (List<?>) content) {
-                                if (obj instanceof Book) {
-                                    System.out.println(((Book) obj).toString());
-                                }
-                            }
-                            System.out.println("\n--- End of recommendations ---\n");
+                        Object content = msg.getContentObject();
+                        if (content instanceof List) {
+                            @SuppressWarnings("unchecked")
+                            List<Book> recs = (List<Book>) content;
+                            // Actualizamos la UI en el EDT
+                            SwingUtilities.invokeLater(() -> ui.showResults(recs));
                         }
                     } catch (UnreadableException e) {
                         e.printStackTrace();
                     }
-                }
-
-                System.out.println("More queries? (y/n)");
-                String resp = scanner.nextLine().trim().toLowerCase();
-                if (resp.equals("n") || resp.equals("no")) {
-                    System.out.println("Closing system");
-                    System.exit(0);
+                } else {
+                    block();
                 }
             }
-        }, "PRINT_RESULTS");
-        fsm.registerDefaultTransition("GET_INPUT", "PRINT_RESULTS");
-        fsm.registerDefaultTransition("PRINT_RESULTS", "GET_INPUT");
-        addBehaviour(fsm);
+        });
+    }
+
+    public void sendPreferencesAndAuthors(Map<String, Integer> prefs, List<String> authors) {
+        ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+        msg.addReceiver(new AID("recommender", AID.ISLOCALNAME));
+        try {
+            msg.setContentObject(new Object[] { prefs, authors });
+            send(msg);
+            System.out.println("Preferences and authors sent to recommender.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
